005b6bf13bdf18d7af86a5365a8b28a4d1f6b5d5
mahdiyar <mahdi2008yar@gmail.com>
2024-02-20 00:34:11 -0800

    aaa

--- 1.js
+++
@@ -1,8 +0,0 @@
-const express = require ("express") ; 
-const app = express () ; 
-
- app.get ('/' , (req, res) => {
-    res.send ('HELLO HElli') ; 
- }); 
-
- app.listen (1147 ,() => console.log ('a')) ;
